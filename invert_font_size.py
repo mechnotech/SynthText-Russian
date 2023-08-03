9fed24863c5887993d58c0c746e1772cd970c91e
mechnotech <eashumilov@fintechiq.ru>
2023-08-03 15:32:08 +0300

    fixed invert_font_size + some bold cyrillic fonts

--- invert_font_size.py
+++ invert_font_size.py
@@ -1,38 +1,35 @@
 # Author: Ankush Gupta
 # Date: 2015
-"Script to generate font-models."
+"""Script to generate font-models."""
 
+import pickle
+
+import numpy as np
 import pygame
 from pygame import freetype
-from text_utils import FontState
-import numpy as np 
-import matplotlib.pyplot as plt 
-import cPickle as cp
 
+from text_utils import FontState
 
 pygame.init()
 
-
-ys = np.arange(8,200)
-A = np.c_[ys,np.ones_like(ys)]
+ys = np.arange(8, 200)
+A = np.c_[ys, np.ones_like(ys)]
 
 xs = []
-models = {} #linear model
+models = {}  # linear model
 
 FS = FontState()
-#plt.figure()
-for i in xrange(len(FS.fonts)):
-	print(i)
-	font = freetype.Font(FS.fonts[i], size=12)
-	h = []
-	for y in ys:
-		h.append(font.get_sized_glyph_height(y))
-	h = np.array(h)
-	m,_,_,_ = np.linalg.lstsq(A,h)
-	models[font.name] = m
-	xs.append(h)
 
-with open('font_px2pt.cp','w') as f:
-	cp.dump(models,f)
-#plt.plot(xs,ys[i])
-#plt.show()
+for i in range(len(FS.fonts)):
+    print(i, FS.fonts[i])
+    font = freetype.Font(FS.fonts[i], size=12)
+    h = []
+    for y in ys:
+        h.append(font.get_sized_glyph_height(float(y)))
+    h = np.array(h)
+    m, _, _, _ = np.linalg.lstsq(A, h, rcond=None)
+    models[font.name] = m
+    xs.append(h)
+
+with open('data/models/font_px2pt.cp', 'wb') as f:
+    pickle.dump(obj=models, file=f)
